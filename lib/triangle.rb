e98739ba098a290ab3bcbc60fcbff60dc81567b1
Robert M Ricci <rmricci121@gmail.com>
2020-01-22 14:35:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,6 @@ class Triangle
   def kind 
   
     if @side_two + @side_two <= @side_three || @side_two + @side_three <= @side_one || @side_three + @side_one <= @side_two
-      
       raise TriangleError
     end
   end
