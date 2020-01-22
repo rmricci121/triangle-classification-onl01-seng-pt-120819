803dd6d3e3a61a3b85d25171e4695258956bf8b5
Robert M Ricci <rmricci121@gmail.com>
2020-01-22 14:54:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     
     if @side_one == @side_two && @side_two == @side_three
       :equilateral
-    elsif @side_one == @side_two && @side_two != @side_three
+    elsif @side_one == @side_two || @side_two == @side_three || @side_one == @side_three
       :isosceles
     end
   end
