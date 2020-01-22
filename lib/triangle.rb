af776bd62b6f439dd4f6d6149faa86609926ff2b
Robert M Ricci <rmricci121@gmail.com>
2020-01-22 14:19:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three  =side_three
+  end
+  
+  def kind 
+  
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
